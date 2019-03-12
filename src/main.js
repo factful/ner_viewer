e0308f3caff2087fdd68a75c52b8fcf515ea9b38
Ted Han <ted@knowtheory.net>
2019-03-12 13:50:19 -0700

    add OCCRP topic.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import App from './App.html';
 
 import whiteFish from './data/new-republic-white-fish.js';
+import deathOnBorder from './data/occrp-death-on-the-border.js';
 
 const app = new App({
 	target: document.body,
 	data: {
 		topics: {
-			whiteFish: whiteFish
+			whiteFish: whiteFish,
+			deathOnBorder: deathOnBorder
 		},
 		activeTopic: "whiteFish",
 		activeService: "google"
