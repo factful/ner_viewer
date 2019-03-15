56155a3e8633086223442a37ce1e461499d0eb31
Ted Han <ted@knowtheory.net>
2019-03-15 13:24:37 -0700

    Rearrange order & default to first entries.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@ import App from './App.html';
 import whiteFish from './data/new-republic-white-fish.js';
 import deathOnBorder from './data/occrp-death-on-the-border.js';
 
+let topics = {
+	whiteFish: whiteFish,
+	deathOnBorder: deathOnBorder
+};
+let initialTopic = Object.keys(topics)[0];
+let initialService = Object.keys(topics[initialTopic])[1];
+
 const app = new App({
 	target: document.body,
 	data: {
-		topics: {
-			whiteFish: whiteFish,
-			deathOnBorder: deathOnBorder
-		},
-		activeTopic: "whiteFish",
-		activeService: "google",
-		activeEntity: null,
-		activeLabel: null
+		topics: topics,
+		activeTopic: initialTopic,
+		activeService: initialService
 	}
 });
 
